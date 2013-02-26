095a2f213a4cb26759972a2fe2f5b592cfd4a38e
Brenton Bostick <bostick@gmail.com>
2013-02-26 11:17:09 -0500

    add Cursor to work back from exit
    for calculating number of move needed

--- Deadlock/src/tools/java/solver/Config.java
+++ Deadlock/src/tools/java/solver/Config.java
@@ -409,6 +409,21 @@ public class Config {
 		}
 	}
 	
+	public static int otherSide(int side) {
+		switch (side) {
+		case 0:
+			return 2;
+		case 1:
+			return 3;
+		case 2:
+			return 0;
+		case 3:
+			return 1;
+		}
+		assert false;
+		return -1;
+	}
+	
 	public char val(int[] coor) {
 		return ini[coor[0]+1][coor[1]+1];
 	}
@@ -659,13 +674,157 @@ public class Config {
 		return val(exit) == 'R';
 	}
 	
+	static class Cursor {
+		Config config;
+		int[] coor;
+		int side;
+		
+		Cursor(Config config) {
+			this.config = config;
+			coor = new int[2];
+			coor[0] = config.exit[0];
+			coor[1] = config.exit[1];
+			side = otherSide(config.side(config.exit));
+		}
+		
+		void move() {
+			switch (side) {
+			case 0:
+				coor[0] = coor[0]-1;
+				if (coor[0] == -1) {
+					assert val() == 'J' || val() == 'K';
+					int[] other = config.otherJoint(coor);
+					int otherSide = config.side(other);
+					switch (otherSide) {
+					case 0:
+						coor[0] = 0;
+						coor[1] = other[1];
+						side = 2;
+						break;
+					case 1:
+						coor[0] = other[0];
+						coor[1] = config.colCount-1;
+						side = 3;
+						break;
+					case 2:
+						coor[0] = config.rowCount-1;
+						coor[1] = other[1];
+						side = 0;
+						break;
+					case 3:
+						coor[0] = other[0];
+						coor[1] = 0;
+						side = 1;
+						break;
+					}
+				}
+				break;
+			case 1:
+				coor[1] = coor[1]+1;
+				if (coor[1] == config.colCount) {
+					assert val() == 'J' || val() == 'K';
+					int[] other = config.otherJoint(coor);
+					int otherSide = config.side(other);
+					switch (otherSide) {
+					case 0:
+						coor[0] = 0;
+						coor[1] = other[1];
+						side = 2;
+						break;
+					case 1:
+						coor[0] = other[0];
+						coor[1] = config.colCount-1;
+						side = 3;
+						break;
+					case 2:
+						coor[0] = config.rowCount-1;
+						coor[1] = other[1];
+						side = 0;
+						break;
+					case 3:
+						coor[0] = other[0];
+						coor[1] = 0;
+						side = 1;
+						break;
+					}
+				}
+				break;
+			case 2:
+				coor[0] = coor[0]+1;
+				if (coor[0] == config.rowCount) {
+					assert val() == 'J' || val() == 'K';
+					int[] other = config.otherJoint(coor);
+					int otherSide = config.side(other);
+					switch (otherSide) {
+					case 0:
+						coor[0] = 0;
+						coor[1] = other[1];
+						side = 2;
+						break;
+					case 1:
+						coor[0] = other[0];
+						coor[1] = config.colCount-1;
+						side = 3;
+						break;
+					case 2:
+						coor[0] = config.rowCount-1;
+						coor[1] = other[1];
+						side = 0;
+						break;
+					case 3:
+						coor[0] = other[0];
+						coor[1] = 0;
+						side = 1;
+						break;
+					}
+				}
+				break;
+			case 3:
+				coor[1] = coor[1]-1;
+				if (coor[1] == -1) {
+					assert val() == 'J' || val() == 'K';
+					int[] other = config.otherJoint(coor);
+					int otherSide = config.side(other);
+					switch (otherSide) {
+					case 0:
+						coor[0] = 0;
+						coor[1] = other[1];
+						side = 2;
+						break;
+					case 1:
+						coor[0] = other[0];
+						coor[1] = config.colCount-1;
+						side = 3;
+						break;
+					case 2:
+						coor[0] = config.rowCount-1;
+						coor[1] = other[1];
+						side = 0;
+						break;
+					case 3:
+						coor[0] = other[0];
+						coor[1] = 0;
+						side = 1;
+						break;
+					}
+				}
+				break;
+			}
+		}
+		
+		char val() {
+			return config.val(coor);
+		}
+		
+	}
+	
 	boolean isClearPathToExit() {
 		
-		cursor = 
+		Cursor cursor = new Cursor(this);
 		while (true) {
-			if (cursor == 'Y' || 'X') {
-				cursor = move cursor
-			} else if (cursor == 'R') {
+			if (cursor.val() == 'Y' || cursor.val() == 'X') {
+				cursor.move();
+			} else if (cursor.val() == 'R') {
 				break;
 			} else {
 				return false;
@@ -677,13 +836,13 @@ public class Config {
 	
 	int numberMovesToWin() {
 		
-		cursor = 
+		Cursor cursor = new Cursor(this); 
 		int moves = 0;
 		while (true) {
-			if (cursor == 'Y' || 'X') {
-				cursor = move cursor
+			if (cursor.val() == 'Y' || cursor.val() == 'X') {
+				cursor.move();
 				moves++;
-			} else if (cursor == 'R') {
+			} else if (cursor.val() == 'R') {
 				break;
 			} else {
 				assert false;
